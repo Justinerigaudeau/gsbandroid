939d0446c8525ae43a0908e8778c16004caba728
Gsln44 <dylan.gosselin44@hotmail.fr>
2016-12-05 16:52:53 +0100

    test

--- app/src/main/java/com/example/rigaudeau/gsbandroid/MainActivity.java
+++ app/src/main/java/com/example/rigaudeau/gsbandroid/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         btnCnx.setOnClickListener(observateurClic);
 
 
-
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
